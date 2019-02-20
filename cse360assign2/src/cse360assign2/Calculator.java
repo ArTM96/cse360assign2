2dc8dc5de0ce48eca94469c0d179286afadb2bef
ArTM96 <42524804+ArTM96@users.noreply.github.com>
2019-02-19 22:19:15 -0700

    Commit 2
    Commit for part 2 of the assignment

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -8,53 +8,66 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-
     /**
      * Description: Creates a calculator object with total of 0.
      */
-	public Calculator () {
-		total = 0;  // not needed - included for clarity
-	}
+    public Calculator() {
+        total = 0;
+    }
+
     /**
      * Description: Returns the total of the calculator
      *
-     * @return 0;
+     * @return total;
      */
-	public int getTotal () {
-		return 0;
-	}
+    public int getTotal() {
+        return total;
+    }
+
     /**
      * Description: Adds the parameter value to the total
      *
      * @param value
      */
-	public void add (int value) {
-		
-	}
+    public void add(int value) {
+        total = total + value;
+        
+    }
+
     /**
      * Description: Subtracts the parameter value to the total
      *
      * @param value
      */
-	public void subtract (int value) {
-		
-	}
-	   /**
+    public void subtract(int value) {
+        total = total - value;
+        
+    }
+
+    /**
      * Description: Multiplies the parameter value to the total
      *
      * @param value
      */
-	public void multiply (int value) {
-		
-	}
+    public void multiply(int value) {
+        total = total * value;
+       
+    }
+
     /**
      * Description: Divides the parameter value to the total
      *
      * @param value
      */
-	public void divide (int value) {
-		
-	}
+    public void divide(int value) {
+        if (value != 0) {
+            total = total / value;
+            
+        } else {
+            total = 0;
+           
+        }
+    }
     /**
     *Description: Returns the history of  the calculator.
     * @return
