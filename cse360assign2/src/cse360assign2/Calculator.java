2bef8f7349675d96be8141d51a6d0ba65d7e8c84
ArTM96 <42524804+ArTM96@users.noreply.github.com>
2019-02-19 20:57:06 -0700

    Commit 1
    First commit in assignment

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,65 @@
+package cse360assign2;
+/**
+*
+* @author Tim
+* @date 2/15/2019
+* @purpose: Calculator class to perform certain calculations.
+*/
+public class Calculator {
+
+	private int total;
+
+    /**
+     * Description: Creates a calculator object with total of 0.
+     */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+    /**
+     * Description: Returns the total of the calculator
+     *
+     * @return 0;
+     */
+	public int getTotal () {
+		return 0;
+	}
+    /**
+     * Description: Adds the parameter value to the total
+     *
+     * @param value
+     */
+	public void add (int value) {
+		
+	}
+    /**
+     * Description: Subtracts the parameter value to the total
+     *
+     * @param value
+     */
+	public void subtract (int value) {
+		
+	}
+	   /**
+     * Description: Multiplies the parameter value to the total
+     *
+     * @param value
+     */
+	public void multiply (int value) {
+		
+	}
+    /**
+     * Description: Divides the parameter value to the total
+     *
+     * @param value
+     */
+	public void divide (int value) {
+		
+	}
+    /**
+    *Description: Returns the history of  the calculator.
+    * @return
+    */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign2/src/cse360assign2/cse360assign2.java
@@ -0,0 +1,7 @@
+package cse360assign2;
+
+public class cse360assign2 {
+	 public static void main(String[] args) {
+	 
+	 }
+}
