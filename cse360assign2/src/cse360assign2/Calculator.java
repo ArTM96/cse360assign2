82dedf847dddf02b909ad3723e34ec63578e81fe
ArTM96 <42524804+ArTM96@users.noreply.github.com>
2019-02-20 17:50:17 -0700

    Commit 3
    Third and final commit for assignment 2.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -2,77 +2,82 @@ package cse360assign2;
 /**
 *
 * @author Tim
-* @date 2/15/2019
+* @date 2/20/2019
 * @purpose: Calculator class to perform certain calculations.
 */
 public class Calculator {
 
-	private int total;
-    /**
-     * Description: Creates a calculator object with total of 0.
-     */
-    public Calculator() {
-        total = 0;
-    }
+   private int total;
+   private String history;
 
-    /**
-     * Description: Returns the total of the calculator
-     *
-     * @return total;
-     */
-    public int getTotal() {
-        return total;
-    }
-
-    /**
-     * Description: Adds the parameter value to the total
-     *
-     * @param value
-     */
-    public void add(int value) {
-        total = total + value;
-        
-    }
-
-    /**
-     * Description: Subtracts the parameter value to the total
-     *
-     * @param value
-     */
-    public void subtract(int value) {
-        total = total - value;
-        
-    }
-
-    /**
-     * Description: Multiplies the parameter value to the total
-     *
-     * @param value
-     */
-    public void multiply(int value) {
-        total = total * value;
-       
-    }
-
-    /**
-     * Description: Divides the parameter value to the total
-     *
-     * @param value
-     */
-    public void divide(int value) {
-        if (value != 0) {
-            total = total / value;
-            
-        } else {
-            total = 0;
-           
-        }
-    }
-    /**
-    *Description: Returns the history of  the calculator.
-    * @return
+   /**
+    * Description: Creates a calculator object with total of 0.
     */
-	public String getHistory () {
-		return "";
-	}
+   public Calculator() {
+       total = 0;
+       history = "0";
+   }
+
+   /**
+    * Description: Returns the total of the calculator
+    *
+    * @return total;
+    */
+   public int getTotal() {
+       return total;
+   }
+
+   /**
+    * Description: Adds the parameter value to the total
+    *
+    * @param value
+    */
+   public void add(int value) {
+       total = total + value;
+       history += " + " + value;
+   }
+
+   /**
+    * Description: Subtracts the parameter value to the total
+    *
+    * @param value
+    */
+   public void subtract(int value) {
+       total = total - value;
+       history += " - " + value;
+   }
+
+   /**
+    * Description: Multiplies the parameter value to the total
+    *
+    * @param value
+    */
+   public void multiply(int value) {
+       total = total * value;
+       history += " * " + value;
+   }
+
+   /**
+    * Description: Divides the parameter value to the total
+    *
+    * @param value
+    */
+   public void divide(int value) {
+       if (value != 0) {
+           total = total / value;
+           history += " / " + value;
+       } else {
+           total = 0;
+           history += " / " + value;
+       }
+   }
+
+   /**
+    * Description: Returns the computational history of the calculator.
+    *
+    * @return history
+    */
+   public String getHistory() {
+       return history;
+   }
 }
--- cse360assign2/src/cse360assign2/cse360assign2.java
+++ cse360assign2/src/cse360assign2/cse360assign2.java
@@ -2,6 +2,23 @@ package cse360assign2;
 
 public class cse360assign2 {
 	 public static void main(String[] args) {
-	 
+	        Calculator Calc = new Calculator();
+	        Calculator myCalculator = new Calculator();
+	        System.out.println("Initial Value: " + Calc.getTotal());
+	        Calc.add(3);
+	        System.out.println("Add 3: " + Calc.getTotal());
+	        Calc.divide(0);
+	        System.out.println("Divide by 0: " + Calc.getTotal());
+	        Calc.subtract(3);
+	        System.out.println("Subtract 3: " + Calc.getTotal());
+	        Calc.divide(0);
+	        System.out.println("Divide by 0: " + Calc.getTotal());
+	        System.out.println(Calc.getHistory());
+
+	        myCalculator.add(4);
+	        myCalculator.subtract(2);
+	        myCalculator.multiply(2);
+	        myCalculator.add(5);
+	        System.out.println(myCalculator.getHistory());
 	 }
 }
